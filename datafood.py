c461c5c1881463a4e345ca15bf1b544b44b372fa
Sem Raka Widyamahendra <99459646+feddd@users.noreply.github.com>
2023-09-27 23:56:56 +0700

    #7 Update datafood.py

--- datafood.py
+++ datafood.py
@@ -9,11 +9,6 @@ import os
 
 # app definition
 app = Flask(__name__)
-
-<<<<<<< HEAD
-# database location
-# DATABASE_PATH = 'C:/Users/u065505/Documents/sandona/mypy/gasfood/food.db'
-
 # database config
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DATABASE_PATH
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:mSVCivGq4m22uLGjZLKD@containers-us-west-204.railway.app:8059/railway'
